1899d51f1c61d4f1aac7884402d10ca170855e98
Arthur Gonçalves <oliveraarthurrtx@gmail.com>
2026-05-14 15:55:57 -0300

    Add new features to the code.

--- src/Desafio.java
+++ src/Desafio.java
@@ -2,15 +2,29 @@ import java.util.Scanner;
 
 public class Desafio {
     public static void main(String[] args) {
-        String nome = "Roberto";
+        Scanner ler = new Scanner(System.in);
+        /*String nome = "Roberto";
         String tipoConta = "Corrente";
-        double saldo = 1599.99;
+        double saldo = 1599.99;*/
+
+        System.out.println("digite o nome do cliente");
+        String nomeCleinte = ler.nextLine();
+        System.out.println("digite o tipo de conta: ");
+        String tipoDaconta = ler.nextLine();
+        System.out.println("digite o saldo da conta: ");
+        double saldoCleinte = ler.nextDouble();
 
         System.out.println("**********************");
+        System.out.println("\nNome do Cliente: " + nomeCleinte);
+        System.out.println("Tipo de Conta: " + tipoDaconta);
+        System.out.println("Saldo da Conta: " + saldoCleinte);
+        System.out.println("\n**********************");
+        
+        /*System.out.println("**********************");
         System.out.println("\nNome do Cliente " + nome);
         System.out.println("Tipo de Conta: " + tipoConta);
         System.out.println("Saldo da Conta: " + saldo);
-        System.out.println("\n**********************");
+        System.out.println("\n**********************");*/
 
         String menu = """
                     ***Digite sua opcao***
@@ -20,7 +34,6 @@ public class Desafio {
                     3 - Receber um valor 
                     4 - Sair 
                 """;
-        Scanner ler = new Scanner(System.in);
 
         int opcao = 0;
         while (opcao != 4) {
@@ -28,20 +41,20 @@ public class Desafio {
             opcao = ler.nextInt();
 
             if (opcao == 1) {
-                System.out.println("O saldo atualizado e "  + saldo);
+                System.out.println("O saldo atualizado e "  + saldoCleinte);
             }else if (opcao == 2) {
                 System.out.println("Qual o valor que deseja transferir ");
                 double valor = ler.nextDouble();
-                if(valor > saldo){
+                if(valor > saldoCleinte){
                     System.out.println("Nao a saldo para transferir");
                 }else {
-                    saldo = saldo - valor;
-                    System.out.println("O novo saldo atualizado e " + saldo);
+                    saldoCleinte = saldoCleinte - valor;
+                    System.out.println("O novo saldo atualizado e " + saldoCleinte);
                 }
             }else if (opcao == 3) {
                 System.out.println("Qual o valor recebido:  ");
                 double valor = ler.nextDouble();
-                saldo += valor;
+                saldoCleinte += valor;
             }else if (opcao != 4) {
                 System.out.println("Opcao invalida");
             }
